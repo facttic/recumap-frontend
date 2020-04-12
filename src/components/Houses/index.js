f80eea0c3c9650944af31d95013488e82f24c55b
Neto Licursi <neto@camba.coop>
2020-04-12 20:18:40 -0300

    completando el formulario de hogares

--- src/components/Houses/index.js
+++ src/components/Houses/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import {
+  Create,
   Datagrid,
   Edit,
   List,
-  Create,
   Show,
   SimpleForm,
   SimpleShowLayout,
@@ -26,6 +26,39 @@ export const HouseList = props => (
     </List>
 );
 
+export const HouseCreate = props => (
+    <Create title="Agregar Hogar" {...props}>
+	<SimpleForm>
+	   <h2>Datos del Responsable</h2>	
+	   <TextInput label="Nombre del responsable" source="responsible_name" />
+	   <TextInput label="Apellido del responsable" source="responsible_surname" />
+ 	   <TextInput label="DNI del responsable" source="responsible_dni" />
+	   <TextInput label="Teléfono del responsable" source="responsible_phone" />
+ 	   <h2>Datos del Hogar</h2>	
+	   <TextInput label="Calle" source="address_street" />
+           <NumberInput label="Número" source="address_number" />
+	   <TextInput label="Barrio" source="address_neighborhood" />	
+	   <TextInput label="Entre calles" source="address_between_streets" />
+	   <TextInput label="Locality" source="address_locality" />
+	   <NumberInput source="lat" />
+	   <NumberInput source="long" />
+	   <h2>Integrantes del hogar</h2>	
+	   <NumberInput label="Canidad de integrantes" source="number_of_members" />	
+	   <NumberInput label="Cantidad de menores en el hogar" source="number_of_minor_members" />
+	   <NumberInput label="Cantidad de adultos en el hogar" source="number_of_older_members" />
+	   <BooleanInput label="¿Algún problema de salud" source="healh_issues" />
+           <h2>Ingresos</h2>
+	   <BooleanInput label="¿Recibe Ingresos de la economía formal?" source="earnings_formal_economy" />
+	   <BooleanInput label="¿Se vieron afectados sus ingresos por el Aislamiento Preventivo Obligatorio?" source="earnings_affected_isolation" />
+	   <BooleanInput label="¿Recibe IFE?" source="family_emergency_plan" />
+	   <BooleanInput label="¿Es beneficiarie de algún plan social?" source="benefited_by_social_plan" />
+	   <h2>Necesidades</h2>
+	   <BooleanInput label="¿Retira bolsón de comida en alguna institución educativa o municipal cercana a su domicilio?" source="attends_community_dining_room" />
+	   <BooleanInput label="¿Asiste a algún comedor?" source="food_help" />
+	</SimpleForm>
+    </Create>
+);
+
 export const HouseShow = props => (
     <Show {...props}>
         <SimpleShowLayout>
@@ -38,7 +71,7 @@ export const HouseShow = props => (
     </Show>
 );
 
-export const HouseCreate = (props) => (
+export const HouseCreate1 = (props) => (
     <Create {...props}>
         <SimpleForm>
             <Grid container spacing={1}>
